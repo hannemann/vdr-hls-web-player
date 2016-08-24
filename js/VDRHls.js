305df7f89f9a19901dbf18a1fdde1e95199a39a5
hannemann <hannemann666@gmx.net>
2016-08-24 22:56:23 +0200

    code style

--- js/VDRHls.js
+++ js/VDRHls.js
@@ -90,7 +90,7 @@ VDRHls.prototype.getHlsController = function () {
             url = this.urlParser.addUniqueParam(url);
         }
         xhr.open('GET', url, true);
-    xhr.setRequestHeader("Authorization", "Basic " + btoa(this.username + ":" + this.password));
+        xhr.setRequestHeader("Authorization", "Basic " + btoa(this.username + ":" + this.password));
 
     }.bind(this);
 
